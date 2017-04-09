3c24ed76ee63c85ae42ad004a46ecc5d129635b9
Quentin Gondat <quentin.gondat@gmail.com>
2017-04-08 19:09:45 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+categories = [
+  "Asiatique",
+  "Américain",
+  "Burgers",
+  "Pizza",
+  "Indien",
+  "Salade",
+  "Thaï",
+  "Libanais",
+  "Equilibrés et délicieux",
+  "Repas d'affaires",
+  "Pâtes Fraîches",
+  "Vietnamien",
+  "Dessert",
+  "Méditerranéen",
+  "Petit déjeuner",
+  "Halal",
+  "Sandwiches",
+  "Glaces",
+  "Rôtisserie",
+  "Boulangerie",
+  "Falafel",
+  "Brasserie",
+  "Healthy",
+  "Couscous",
+  "Japonais",
+  "Mexicain",
+  "Bobun",
+  "Poulet",
+  "Oriental",
+  "Européen",
+  "Sushi",
+  "Africain",
+  "Chinois",
+  "Bagels",
+  "Tapas",
+  "Syrien",
+  "Allemand",
+  "Argentin",
+  "Empanadas",
+  "Sud Américain",
+  "Réunionais",
+  "Dim Sum",
+  "Fusion Asiatique",
+  "Végétarien",
+  "Pakistanais",
+  "Tibétain",
+  "Fruits de mer",
+  "Crêperie",
+  "Coréen",
+  "Bistronomie",
+  "Tacos",
+  "Burritos"
+]
+
+categories.each do |cat|
+  category = Category.new(name: cat)
+  category.save!
+end
+
+
